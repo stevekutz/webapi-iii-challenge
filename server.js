0c00b437749d5224949b13227ac8dbe3dc33c367
stevekutz <stkutz@gmail.com>
2019-05-28 20:58:08 -0500

    verified functionality of 3rd party middleware

--- server.js
+++ server.js
@@ -5,22 +5,34 @@ const logger = require('morgan');  // logger thingy
 
 
 // ADD this here and start building postRouter.js
-const HubsRouter = require('./posts/postRouter');
+const PostsRouter = require('./posts/postRouter');
 
 
 
 const server = express();
 
-// *-> GLOBAL MIDDLEWARE(declared up top here)
+// *******-> GLOBAL MIDDLEWARE(declared up top here)
 //* this is built-in middleware !!!
 server.use(express.json());    // allows reading / parses req.body !!!!
+
+
+
+// @@@@@@@->  third-party-middleware
+server.use(helmet());
+// >>> morgan pre-define formats: combined, common, dev, short, tiny
+//server.use(logger('short')); // include parameter for logging display option
+server.use(logger('dev'));
+
+
+// Postsrouter endpoint called out
+server.use('api/posts', PostsRouter);
 
 
 server.get('/', (req, res) => {
   res.send(`<h2>Routes and custom middleware !</h2>`)
 });
 
-//custom middleware
+//#######->  custom middleware
 
 function logger2(req, res, next) {
 
