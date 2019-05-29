a3cdb969655e3139cc2dccccb7abc7727e6f8f73
stevekutz <stkutz@gmail.com>
2019-05-28 22:56:46 -0500

    GET /users/:id working

--- users/userRouter.js
+++ users/userRouter.js
@@ -29,9 +29,22 @@ router.get('/', async (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
-
-});
+router.get('/:id', async (req, res) => {
+    try {
+      const user = await Users.getById(req.params.id);
+  
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: `User with id ${id} not found` });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: `Error retrieving the user`,
+      });
+    }
+  });
 
 router.get('/:id/posts', (req, res) => {
 
